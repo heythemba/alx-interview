c4a15e8aa4bff540daf3c708c7dbab5794fc7941
Haythem <heythem.94@gmail.com>
2024-10-18 11:13:08 +0100

    pep8 validation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -21,10 +21,10 @@ def minOperations(n):
 
     return operations
 
+
 if __name__ == "__main__":
     n = 4
     print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
     n = 12
     print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-
