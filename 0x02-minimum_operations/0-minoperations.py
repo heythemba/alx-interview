6d5b4ff3eb15f5ed7e39da10ccf5dbeb2dc43946
Haythem <heythem.94@gmail.com>
2024-10-18 11:21:29 +0100

    pep8 validation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """
-Module for minimum operations problem
+Module for minimum operations problem.
 """
 
-
-def minOperations(n):
+def min_operations(n):
     """
     Calculates the fewest number of operations needed
     to result in exactly n H characters.
+    
+    Args:
+        n (int): The number of H characters to achieve.
+    
+    Returns:
+        int: The minimum number of operations required.
     """
     if not isinstance(n, int) or n <= 0:
         return 0
@@ -26,7 +31,7 @@ def minOperations(n):
 
 if __name__ == "__main__":
     n = 4
-    print("Min # of ope. to reach {} char: {}".format(n, minOperations(n)))
+    print("Min # of operations to reach {} chars: {}".format(n, min_operations(n)))
 
     n = 12
-    print("Min # of ope. to reach {} char: {}".format(n, minOperations(n)))
+    print("Min # of operations to reach {} chars: {}".format(n, min_operations(n)))
